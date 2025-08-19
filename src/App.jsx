a2047ad802fcd246aeb8f6f39e8419d361d21908
Paola Ortiz de Zevallos <paolaozv@gmail.com>
2025-08-19 16:41:25 +0200

    Added structure movie project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { use, useEffect, useState } from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <main>
+			<div className='pattern' />
+			<div className='wrapper'>
+				<header>
+					<h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+				</header>
+			</div>
+    </main>
+  )
+}
+
+export default App
